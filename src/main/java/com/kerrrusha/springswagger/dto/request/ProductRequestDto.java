0dd18b8fc1be8f37745a3ebd56fcf65b4b004dda
Kirill Koval <brcyrill2@gmail.com>
2023-06-13 20:51:17 +0300

    added product dtos

---
+++ src/main/java/com/kerrrusha/springswagger/dto/request/ProductRequestDto.java
@@ -0,0 +1,12 @@
+package com.kerrrusha.springswagger.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class ProductRequestDto {
+
+    private String title;
+
+    private Long price;
+
+}
---
+++ src/main/java/com/kerrrusha/springswagger/dto/response/ProductResponseDto.java
@@ -0,0 +1,16 @@
+package com.kerrrusha.springswagger.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductResponseDto {
+
+    private Long id;
+
+    private String title;
+
+    private Long price;
+
+}
